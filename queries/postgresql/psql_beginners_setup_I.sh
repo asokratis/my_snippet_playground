0acb4a22290c8f196741dbe17222d71d6ae948fb
asokratis <asokratis@gmail.com>
2017-08-09 11:15:36 +0700

    Update psql_beginners_setup_I.sh

--- queries/postgresql/psql_beginners_setup_I.sh
+++ queries/postgresql/psql_beginners_setup_I.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get install postgresql postgresql-contrib
 sudo -u postgres psql -c "create schema hackerrank"
 sudo -u postgres psql -c "\i '/home/cabox/workspace/menu_ddt.sql';"
-sudo -u postgres psql -c "\copy hackerrank.mcdonalds_menu FROM '/home/cabox/workspace/menu.csv' DELIMITER ',' CSV HEADER QUOTE"
-sudo -u postgres psql -c "\copy (Select * From hackerrank.mcdonalds_menu where lower(category)='salads') To '../tmp/mcdonalds_salads.txt' DELIMITER ',' NULL '' With CSV HEADER QUOTE;"
+sudo -u postgres psql -c "\copy hackerrank.mcdonalds_menu FROM '/home/cabox/workspace/menu.csv' DELIMITER ',' CSV HEADER"
+sudo -u postgres psql -c "\copy (SELECT * FROM hackerrank.mcdonalds_menu where lower(category)='salads') TO '/tmp/mcdonalds_salads.txt' NULL '' DELIMITER ',' CSV HEADER FORCE QUOTE *;"
 sudo cp -R ../tmp/ /home/cabox/workspace/results/
 sudo -u postgres psql
