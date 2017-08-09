8534198349da4fe29c64003161e5dcbfc28e511a
asokratis <asokratis@gmail.com>
2017-08-09 11:45:40 +0700

    Update psql_beginners_setup_I.sh

--- queries/postgresql/psql_beginners_setup_I.sh
+++ queries/postgresql/psql_beginners_setup_I.sh
@@ -5,5 +5,5 @@ sudo -u postgres psql -c "create schema hackerrank"
 sudo -u postgres psql -c "\i '/home/cabox/workspace/menu_ddt.sql';"
 sudo -u postgres psql -c "\copy hackerrank.mcdonalds_menu FROM '/home/cabox/workspace/menu.csv' DELIMITER ',' CSV HEADER"
 sudo -u postgres psql -c "\copy (SELECT * FROM hackerrank.mcdonalds_menu where lower(category)='salads') TO '/tmp/mcdonalds_salads.txt' NULL '' DELIMITER ',' CSV HEADER FORCE QUOTE *;"
-sudo cp -R ../tmp/ /home/cabox/workspace/results/
+sudo cp -R /tmp/ /home/cabox/workspace/results/
 sudo -u postgres psql
